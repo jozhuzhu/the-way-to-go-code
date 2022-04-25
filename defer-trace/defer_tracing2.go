f404d165e9e5591dc4d589495f978061c666447b
jozhuzhu <jozhuzhu@yandex.com>
2022-04-25 14:38:52 +0800

    first commit

---
+++ defer-trace/defer_tracing2.go
@@ -0,0 +1,24 @@
+package defer_trace
+
+import "fmt"
+
+func trace(s string) string {
+	fmt.Println("entering:", s)
+	return s
+}
+
+func un(s string) {
+	fmt.Println("leaving:", s)
+}
+
+func a() {
+	defer un(trace("a"))
+	fmt.Println("in a")
+}
+
+func B() {
+	// defer 影响的是最外层函数
+	defer un(trace("b"))
+	fmt.Println("in b")
+	a()
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import defer_trace "defer-trace"
+
+func main() {
+	defer_trace.B()
+}
